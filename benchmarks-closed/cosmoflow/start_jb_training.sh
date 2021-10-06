459b67c84299ff9376c4cfb3721c6c4ef4299e6d
janEbert <janpublicebert@posteo.net>
2021-10-06 13:21:33 +0200

    Fix delayed shard multiplier doubling
    Previously was interpolated and in the end wasn't delayed at all.

--- benchmarks-closed/cosmoflow/start_jb_training.sh
+++ benchmarks-closed/cosmoflow/start_jb_training.sh
@@ -67,7 +67,7 @@ srun "${SRUN_PARAMS[@]}" singularity exec --nv \
       PMIX_SECURITY_MODE=native; \
       HOME=''; \
       source ${CONFIG_FILE}; \
-      export DATA_SHARD_MULTIPLIER=$((DATA_SHARD_MULTIPLIER * 2)); \
+      export DATA_SHARD_MULTIPLIER=\$((\$DATA_SHARD_MULTIPLIER * 2)); \
       export DGXNNODES=$SLURM_NNODES; \
       export DGXNGPU=4; \
       bash run_and_time.sh"
