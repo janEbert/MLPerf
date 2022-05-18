152c6e26923b52ecd4037b5fbe01c8e4c081d701
janEbert <janpublicebert@posteo.net>
2022-05-18 14:00:03 +0200

    Adjust data paths

--- HelmholtzAI/benchmarks/implementations/deepcam/run_scripts/run_jb24x128_hpst.sh
+++ HelmholtzAI/benchmarks/implementations/deepcam/run_scripts/run_jb24x128_hpst.sh
@@ -3,6 +3,6 @@ export TRAINING_INSTANCE_SIZE=128
 export NINSTANCES=24
 export STAGE_DIR_PREFIX=/tmp/deepcam
 export STAGE_ONLY=0
-export DATA_DIR_PREFIX="/p/cscratch/fs/hai_mlperf/deepCam2/"
+export DATA_DIR_PREFIX="/p/cscratch/fs/hai_mlperf/deepcam_hdf5/"
 CONFIG=configs/best_configs/config_DGXA100_128GPU_BS128_graph.sh
 ./start_training_run.sh -s booster -N $((TRAINING_INSTANCE_SIZE/4*NINSTANCES)) -c $CONFIG  -t 00:35:00
--- HelmholtzAI/benchmarks/implementations/deepcam/run_scripts/run_jb512_gpfs.sh
+++ HelmholtzAI/benchmarks/implementations/deepcam/run_scripts/run_jb512_gpfs.sh
@@ -2,6 +2,6 @@
 export TRAINING_INSTANCE_SIZE=512
 export STAGE_DIR_PREFIX=/tmp/deepcam
 export STAGE_ONLY=0
-#export DATA_DIR_PREFIX="/p/cscratch/fs/hai_mlperf/deepCam2/"
+#export DATA_DIR_PREFIX="/p/cscratch/fs/hai_mlperf/deepcam_hdf5/"
 CONFIG=configs/best_configs/config_DGXA100_512GPU_BS1024_graph.sh 
 ./start_training_run.sh -s booster -N $((TRAINING_INSTANCE_SIZE/4)) -c $CONFIG  -t 00:10:00
